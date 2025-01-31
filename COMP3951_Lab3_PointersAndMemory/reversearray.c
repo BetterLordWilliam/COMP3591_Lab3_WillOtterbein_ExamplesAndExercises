743d6e2b69b0de262df31ab8a4714f44c7a3e4c7
LordWilliam <willjordan05@icloud.com>
2025-01-31 10:15:21 -0800

    WO: Implemented C version of the unsafe program to reverse array of integers.

---
+++ COMP3951_Lab3_PointersAndMemory/reversearray.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printIntArray(int* arrHead, int arrLength)
+{
+	printf("[%d", *(arrHead + 0));
+	for (int i = 1; i < arrLength; i++)
+	{
+		printf(",%d", *(arrHead + i));
+	}
+	printf("]\n");
+}
+
+// Reverse an integer array using pointers 
+// This is the method you need to implement in C#
+void reverseIntArray(int* arrHead, int arrLength)
+{
+	for (int i = 0; i < arrLength; i++)
+	{
+		int* temp = *(arrLength + i);
+		*(arrLength + i) = *(arrLength + arrLength - i - 1);
+		*(arrLength + arrLength - i - 1) = *temp;
+	}
+}
+
+int main(int argc, char** argv)
+{
+	int* integerArray = [10,20,30,40,50];
+	int integerArraySize = sizeof(integerArray) / sizeof(int);
+	printf("%d", integerArraySize);
+}
